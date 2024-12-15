be49ecfbd5be50d3442fb07b3ed6469098ea5dad
erikamc99 <erikamc99@gmail.com>
2024-12-15 18:38:07 +0100

    filter function

--- kamakura-food/src/menu.js
+++ kamakura-food/src/menu.js
@@ -1,5 +1,35 @@
 import { products } from '../assets/data/data.js';
+import { filterProducts } from './searcher.js';
+
 //DEBE imprimir en pantalla la información de filtros.
+
+const filterContainer = document.getElementById('filters');
+const productContainer = document.getElementById('products');
+
+// Obtener solo 1 categoría
+function getFilterCategories(products) {
+    const categories = products.map(product => product.category);
+    return ['todos', ...new Set(categories)]; 
+}
+
+// Mostrar los filtros
+
+function getFilters() {
+    const categories = getFilterCategories(products);
+    const filterButtons = categories.map(category => `
+        <button class="filter" data-category="${category}">${category}</button>
+    `).join('');
+    filterContainer.innerHTML = filterButtons;
+    // Llamar a los botones por separado
+    const filterButtonsElements = document.querySelectorAll('.filter');
+    filterButtonsElements.forEach(button => {
+        button.addEventListener('click', () => {
+            const category = button.getAttribute('data-category');
+            const filteredProducts = filterProducts(products, category, getFullProducts); // Estas dos líneas también están cambiadas para adaptarlo, por eso no funcionaba
+            productContainer.innerHTML = filteredProducts; // Soy poco original con los nombres, lo sé puede haber lío porque en searcher es filteredProduct (:
+        });
+    });
+}
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
 
@@ -15,3 +45,7 @@ function getFullProducts(item) {
         </div>
     `;
 }
+
+// Llamadas para que se muestre todo (Tal vez deberíamos ir planteándonos eso de ir cambiando a events.js)
+getFilters();
+productContainer.innerHTML = filterProducts(products, 'todos', getFullProducts); // Lo he tenido que cambiar para que funcione, si os fijais se reemplaza el 'render' por la función que queremos
