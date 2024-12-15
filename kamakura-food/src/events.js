857f77c7b9aeb65ae022a5ecf56aa9346413482b
LannyRivero <lannyrivero288@gmail.com>
2024-12-15 18:26:00 +0100

    menu products

--- kamakura-food/src/events.js
+++ kamakura-food/src/events.js
@@ -1,2 +1,11 @@
+
+import {products} from '../assets/data/data.js'
+import { getFullProducts } from './menu.js';
 //Intenta separar los eventos en este archivo.
 
+function init() {    
+    getFullProducts(products);
+    initCart();
+}
+
+init();
