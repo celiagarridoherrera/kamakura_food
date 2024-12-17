6e4760f3200332e7d8181a32a19be98cd26cc52e
erikamc99 <erikamc99@gmail.com>
2024-12-17 09:39:59 +0100

    modify functions & create events

--- kamakura-food/src/events.js
+++ kamakura-food/src/events.js
@@ -1,11 +1,30 @@
-
-import {products} from '../assets/data/data.js'
-import { getFullProducts } from './menu.js';
 //Intenta separar los eventos en este archivo.
 
-function init() {    
-    getFullProducts(products);
-    initCart();
+import {products} from '../assets/data/data.js';
+import {getFilters, getFullProducts} from './menu.js';
+import {filterProducts} from './searcher.js';
+
+function init() {
+    let filterContainer = document.getElementById('filters');
+    let productContainer = document.getElementById('products');
+
+    // MOSTRAR FILTROS Y PRODUCTOS
+    getFilters(filterContainer);
+
+    productContainer.innerHTML = filterProducts(products, 'todos', getFullProducts);
+
+    filterContainer.addEventListener('click', (event) => handleFilterClick(event, productContainer));
+
+    // AQUÍ SE AÑADIRÍAN LOS DEMÁS EVENTOS DE CART Y TICKET
+}
+
+// CLICKS DE LOS BOTONES (MÁS REUTILIZABLE, P. EJ. CARRITO)
+function handleFilterClick(event, productContainer) {
+    if (event.target.classList.contains('filter')) {
+        let category = event.target.getAttribute('data-category');
+        let filteredProducts = filterProducts(products, category, getFullProducts);
+        productContainer.innerHTML = filteredProducts;
+    }
 }
 
 init();
